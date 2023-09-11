b1d9b42f4b4a2854eddded3fc713e79b84accdc7
rajon-git <rajon.zhsust15@gmail.com>
2023-09-12 00:00:18 +0600

    auth reducers for admin login

--- dashboard/src/views/auth/AdminLogin.jsx
+++ dashboard/src/views/auth/AdminLogin.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { admin_login } from "../../store/Reducers/authReducer";
 
 const AdminLogin = () => {
+    const dispatch= useDispatch()
     const [state,setState]=useState({
         email:"",
         password:""
@@ -15,7 +18,7 @@ const AdminLogin = () => {
     
       const submit=(e)=>{
         e.preventDefault();
-        console.log(state)
+        dispatch(admin_login(state))
       }
   return (
     <div className="min-w-screen min-h-screen bg-[#161d31] flex justify-center items-center">
