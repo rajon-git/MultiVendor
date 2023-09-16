e03278482af56f1486ce31e9b2d88c56dea1f277
rajon-git <rajon.zhsust15@gmail.com>
2023-09-17 00:05:29 +0600

    header serach, name,role,photo add

--- dashboard/src/layout/MainLayout.jsx
+++ dashboard/src/layout/MainLayout.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from './Header'
 import SideBar from './Sidebar'
 
 const MainLayout = () => {
+  const [showSidebar, setShowSidebar] =useState(false)
   return (
     <div className='bg-[#161d31] w-full min-h-screen'>
-      <Header/>
-      <SideBar/>
+      <Header showSidebar={showSidebar} setShowSidebar={setShowSidebar}/>
+      <SideBar showSidebar={showSidebar} setShowSidebar={setShowSidebar}/>
       <div className='ml-0 lg:ml-[260px] pt-[95px] transition-all'>
         <Outlet/>
       </div>
