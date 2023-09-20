86c4b9a535d555de30bffa71691fa6fee41f1731
rajon-git <rajon.zhsust15@gmail.com>
2023-09-20 23:51:45 +0600

    order page pagination complete #15

--- dashboard/src/views/admin/Orders.jsx
+++ dashboard/src/views/admin/Orders.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { BsArrowBarDown } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import Pagination from "../Pagination";
 
 const Orders = () => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -97,6 +98,15 @@ const Orders = () => {
               </div>
             </div>
           </div>
+        </div>
+        <div className="w-full flex  justify-end mt-4 bottom-4 right-4 ">
+        <Pagination
+        pageNumber={currentPage}
+        setPageNumber={setCurrentPage}
+        totalItem={50}
+        perPage={perPage}
+        showItem={3}
+        />
         </div>
       </div>
     </div>
